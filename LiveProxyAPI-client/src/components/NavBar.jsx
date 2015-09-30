a565463b2267efa720df3ed731e925774e20a95d
Marcus Bernales <mboperator@gmail.com>
2015-09-29 22:53:39 -0700

    Aesthetic things

--- LiveProxyAPI-client/src/components/NavBar.jsx
+++ LiveProxyAPI-client/src/components/NavBar.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {
+  Navbar, Nav, NavItem,
+  NavDropdown, MenuItem
+} from 'react-bootstrap';
 
 export default class NavBar extends React.Component {
   render() {
     return (
       <Navbar brand='LiveProxy'>
-        <Nav>
+        <Nav right>
             <NavItem eventKey={1} href='#/viewer'>
                 Server State
             </NavItem>
             <NavItem eventKey={1} href='#/posts'>
                 Posts
             </NavItem>
+            <NavDropdown title='JSON API'>
+              <MenuItem href='http://localhost:8091/api/mock/posts'>
+                Posts
+              </MenuItem>
+
+              <MenuItem href='http://localhost:8091/api/mock/comments'>
+                Comments
+              </MenuItem>
+
+              <MenuItem href='http://localhost:8091/api/mock/users'>
+                Users
+              </MenuItem>
+            </NavDropdown>
         </Nav>
       </Navbar>
     );
