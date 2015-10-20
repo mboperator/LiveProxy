a99c5583d13f58ab4c4f3e9e915c85bdea678d0f
Marcus Bernales <mboperator@gmail.com>
2015-10-19 22:19:04 -0700

    cleaned routes

--- server/src/services/routes.js
+++ server/src/services/routes.js
@@ -1,24 +1,20 @@
-import makeStore from '../store';
+import express from 'express';
 import * as apiRequest from './apiRequest';
+import * as resourceActions from '../actions/resource';
 import story from '../../definitions/story';
 import sentence from '../../definitions/sentence';
-import * as resourceActions from '../actions/resource';
-import express from 'express';
-
-var router = express.Router();
-
-const storyRoute = '/stories';
-const sentenceRoute = '/sentences';
 
 export default function createRouter(store) {
+  var router = express.Router();
 
-  router.get(storyRoute, function (req, res) {
+  router.get('/stories', (req, res) => {
+    console.log('stories#index');
     const resources = store.getState().getIn(['collections', story.name]).toList().toJS();
     res.send({[story.keys.plural]: resources});
   });
 
-  router.delete('/stories/:id', function (req, res) {
-    console.log('Deleting', req.params.id);
+  router.delete('/stories/:id', (req, res) => {
+    console.log('stories#delete', req.params.id);
     const actionRequest = {def: story, id: req.params.id};
     const action = resourceActions['DESTROY_RESOURCE'](actionRequest);
     const { type, payload } = action;
@@ -34,8 +30,8 @@ export default function createRouter(store) {
     });
   });
 
-  router.post(storyRoute, function (req, res) {
-    console.log('CREATE', req.body);
+  router.post('/stories', (req, res) => {
+    console.log('stories#create', req.body);
     const actionRequest = {def: story, doc: req.body};
     const action = resourceActions['CREATE_RESOURCE'](actionRequest);
     const { type, payload } = action;
@@ -50,14 +46,16 @@ export default function createRouter(store) {
         readyState: 'success',
       });
     });
-
   });
 
-  router.patch(storyRoute, function (req, res) {
+  router.patch('/stories', (req, res) => {
+    console.log('stories#patch', req.body);
     const actionRequest = {def: story, doc: req.body};
     const action = resourceActions['PATCH_RESOURCE'](actionRequest);
     const { type, payload } = action;
+
     apiRequest.patch(actionRequest).then( (data) => {
+      res.send(data);
       store.dispatch({
         type: `${type}_SUCCESS`,
         meta: action.meta,
@@ -65,8 +63,8 @@ export default function createRouter(store) {
         result: data,
         readyState: 'success',
       });
-      res.send(data)
-    })
+    });
   });
+
   return router;
-};
+}
